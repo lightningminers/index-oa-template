d6a8d658e84afdee6ecbb513fd51a523d8be9fb5
icepy <xiangwenwe@foxmail.com>
2017-03-27 14:39:20 +0800

    演示处理vue 2.0 weex Demo

--- src/weex.entry.js
+++ src/weex.entry.js
@@ -1,8 +1,8 @@
-// import Hello from './Hello.vue';
-// Hello.el = '#app';
-// new Vue(Hello);
+import Hello from './Hello.vue';
+Hello.el = '#app';
+new Vue(Hello);
 
 
-import Home from './pages/home/index.vue';
-Home.el = '#app';
-new Vue(Home);
+// import Home from './pages/home/index.vue';
+// Home.el = '#app';
+// new Vue(Home);
